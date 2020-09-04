233b146bd65ffd59346f7bc5adf74f25e9221e17
Hardikgithub2020 <hardik.unity@gmail.com>
2020-09-04 16:38:22 -0400

    Work on 9/4/2020

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Section1 from './Components/Section1';
+import Section2 from './Components/Section2';
+import Section3 from './Components/Section3';
+import Section4 from './Components/Section4';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Section1/>
+      <Section2/>
+      <Section3/>
+      <Section4/>
+
+
     </div>
   );
 }
---
+++ src/Components/Section1.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Section1 extends Component {
+    render() {
+        return (
+            <div className='Section1'>
+                <img src="https://media2.giphy.com/media/l2Je32EwtM8jUnKk8/200_d.gif"/>
+                
+            </div>
+        );
+    }
+}
+
+export default Section1;
